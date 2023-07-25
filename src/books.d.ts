03067442cb8bde1b432d37c1fcdae2edb3e18f4d
MathiTarazonad <vladitarazona@gmail.com>
2023-07-25 09:51:29 -0400

    Create initial types for books

---
+++ src/books.d.ts
@@ -0,0 +1,17 @@
+export type Library = Book[]
+
+export interface Book {
+  book: {
+    title: string
+    pages: number
+    genre: string
+    cover: string
+    synopsis: string
+    year: number
+    ISBN: string
+    author: {
+      name: string
+      otherBooks: string[]
+    }
+  }
+}
