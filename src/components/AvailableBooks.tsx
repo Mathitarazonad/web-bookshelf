fac51b0eb20f2555cdb9e992a0abd790e6010b0c
MathiTarazonad <vladitarazona@gmail.com>
2023-07-26 14:15:23 -0400

    Add functionality to render based on filters

--- src/components/AvailableBooks.tsx
+++ src/components/AvailableBooks.tsx
@@ -1,13 +1,24 @@
+import { useEffect, useState } from 'react'
 import useLibrary from '../hooks/useLibrary'
 import BookView from './BookView'
+import filterStore from '../store/filterStore'
+import { filterLibrary } from '../services/books'
 
 export default function AvailableBooks () {
   const { availableBooks } = useLibrary()
+  const { selectedGenres, pages } = filterStore()
+  const [updatedAvailableBooks, setUpdatedAvailableBooks] = useState([...availableBooks])
+
+  useEffect(() => {
+    let filteredLibrary = filterLibrary('genre', selectedGenres, availableBooks)
+    filteredLibrary = filterLibrary('pages', pages, filteredLibrary)
+    setUpdatedAvailableBooks(filteredLibrary)
+  }, [selectedGenres, availableBooks, pages])
 
   return (
-    <ul className='flex gap-4 flex-wrap'>
-      {availableBooks.map(({ book }) =>
+    <section className='flex gap-4 flex-wrap'>
+      {updatedAvailableBooks.map(({ book }) =>
         <BookView key={book.ISBN} book={book} />)}
-    </ul>
+    </section>
   )
 }
