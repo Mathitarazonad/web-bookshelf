05d848d07b54bff82b9f81ff86e390a875d29e91
MathiTarazonad <vladitarazona@gmail.com>
2023-07-26 13:51:37 -0400

    Change pages type and use localStorage services

--- src/store/filterStore.ts
+++ src/store/filterStore.ts
@@ -1,28 +1,24 @@
 import { create } from 'zustand'
+import { getFromLocalStorage, updateLocalStorage } from '../services/localStorage'
 
 interface FilterState {
   selectedGenres: string[]
   updateSelectedGenres: (genres: string[]) => void
-  pages: string | number
-  updatePagesFilter: (newPages: number) => void
-}
-
-const getFromLocalStorage = (item: string) => {
-  if (window.localStorage.getItem(item) !== null) {
-    return JSON.parse(window.localStorage.getItem(item) as string)
-  }
-
-  return null
+  pages: string
+  updatePagesFilter: (newPages: string) => void
 }
 
 const filterStore = create<FilterState>(set => ({
   selectedGenres: getFromLocalStorage('selectedGenres') ?? [],
   updateSelectedGenres: (genres) => set(() => {
-    window.localStorage.setItem('selectedGenres', JSON.stringify(genres))
+    updateLocalStorage('selectedGenres', genres)
     return { selectedGenres: [...genres] }
   }),
-  pages: getFromLocalStorage('pages') ?? 0,
-  updatePagesFilter: (newPages: number) => set(() => ({ pages: newPages }))
+  pages: getFromLocalStorage('pages') ?? '0',
+  updatePagesFilter: (newPages: string) => set(() => {
+    updateLocalStorage('pages', newPages)
+    return { pages: newPages }
+  })
 }))
 
 export default filterStore
