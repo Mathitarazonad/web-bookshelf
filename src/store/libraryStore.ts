eaaba1c8628cfb168bea8b2395392b74d90872c1
MathiTarazonad <vladitarazona@gmail.com>
2023-07-26 09:47:59 -0400

    Create initial library store

---
+++ src/store/libraryStore.ts
@@ -0,0 +1,27 @@
+import { create } from 'zustand'
+import { getLibrary } from '../services/books'
+import { Library, LibraryType } from '../books'
+
+export interface LibraryState {
+  availableBooks: Library
+  toRead: Library
+  updateLibrary: (libraryType: LibraryType, library: Library) => void
+}
+
+const getFromLocalStorage = (item: LibraryType) => {
+  if (window.localStorage.getItem(item) !== null) {
+    return JSON.parse(window.localStorage.getItem(item) as string)
+  }
+
+  return null
+}
+
+const libraryStore = create<LibraryState>((set) => ({
+  availableBooks: getFromLocalStorage('availables') ?? getLibrary(),
+  toRead: getFromLocalStorage('toRead') ?? [],
+  updateLibrary: (libraryType: LibraryType, library: Library) => set(state => ({
+    ...state, [libraryType]: library
+  }))
+}))
+
+export default libraryStore
