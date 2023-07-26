a7d9a6b1d211b18fd1c88c02e909cc4fb74b0a8c
MathiTarazonad <vladitarazona@gmail.com>
2023-07-26 14:14:40 -0400

    Add functionality to switch between libraries

--- src/components/BookView.tsx
+++ src/components/BookView.tsx
@@ -1,6 +1,24 @@
 import { Book } from '../books'
+import useLibrary from '../hooks/useLibrary'
 
 export default function BookView ({ book }: Book) {
+  const { availableBooks, toRead, updateLibraries } = useLibrary()
+  const isReading = toRead.filter(({ book: b }) => b.ISBN === book.ISBN).length === 1
+
+  const handleLibraryChange = () => {
+    if (isReading) {
+      const updatedAvailableBooks = [...availableBooks, { book }]
+      const updatedReadingLibrary = toRead.filter(({ book: b }) => b.ISBN !== book.ISBN)
+      updateLibraries('availableBooks', updatedAvailableBooks)
+      updateLibraries('toRead', updatedReadingLibrary)
+      return
+    }
+    const updatedAvailableBooks = availableBooks.filter(({ book: b }) => b.ISBN !== book.ISBN)
+    const updatedReadingLibrary = [...toRead, { book }]
+    updateLibraries('availableBooks', updatedAvailableBooks)
+    updateLibraries('toRead', updatedReadingLibrary)
+  }
+
   return (
     <article className='flex flex-col w-[300px]'>
       <header className='flex gap-2'>
@@ -15,7 +33,9 @@ export default function BookView ({ book }: Book) {
         </section>
       </header>
       <footer>
-        <button>Move to read</button>
+        <button type='button' onClick={() => handleLibraryChange()}>
+          {isReading ? 'Remove from list' : 'Move to read'}
+        </button>
       </footer>
     </article>
   )
