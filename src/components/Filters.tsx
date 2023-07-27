d17bd8d9a9d5176ae26d1042b835cfcb2263a6a8
MathiTarazonad <vladitarazona@gmail.com>
2023-07-27 19:49:38 -0400

    Fix bug and some styles

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -11,6 +11,10 @@ export default function Filters () {
     return availableBooks.reduce((prev, { book }) => book.pages > prev ? book.pages : prev, 0)
   }, [availableBooks])
 
+  useEffect(() => {
+    if (parseInt(pages) > maxNumberOfPages) updatePagesFilter(JSON.stringify(maxNumberOfPages))
+  }, [maxNumberOfPages])
+
   const handleGenreChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { checked: checkState, value } = e.target
     if (checkState) {
@@ -32,35 +36,38 @@ export default function Filters () {
   }, [])
 
   return (
-    <div className='col-span-1 flex flex-col w-full h-max border-r-2 border-amber-950 border-opacity-10 '>
-      <h3 className='text-2xl text-amber-950 font-semibold'>Filter by</h3>
-      <fieldset>
-        <legend>Genre</legend>
-        {genres.map(gen =>
-          <div className='flex gap-2' key={gen}>
-            <label htmlFor={gen}>
-              {gen} ({availableBooks.filter(({ book }) => book.genre === gen).length})
-            </label>
-            <input
-              type='checkbox'
-              id={gen}
-              value={gen}
-              onChange={handleGenreChange}
-              checked={selectedGenres.includes(gen)}
-            />
-          </div>)}
-      </fieldset>
-      <fieldset>
-        <label htmlFor='pages'>Pages</label>
-        <input
-          type='range'
-          min={0} max={maxNumberOfPages}
-          id='pages'
-          onChange={handlePageChange}
-          value={parseInt(pages) > maxNumberOfPages ? maxNumberOfPages : pages}
-        />
-        <p>{pages} min pages</p>
-      </fieldset>
+    <div className='col-span-1 flex flex-col w-full h-max border-r-2 border-orange-800 border-opacity-10 '>
+      <h3 className='text-2xl text-orange-800 font-bold'>Filter by</h3>
+      <div className='flex flex-col gap-2'>
+        <fieldset>
+          <legend className='text-orange-800 font-semibold text-lg'>Genre</legend>
+          {genres.map(gen =>
+            <div className='flex gap-2' key={gen}>
+              <input
+                type='checkbox'
+                id={gen}
+                value={gen}
+                onChange={handleGenreChange}
+                checked={selectedGenres.includes(gen)}
+              />
+              <label htmlFor={gen} className='text-gray-600 font-medium'>
+                {gen} ({availableBooks.filter(({ book }) => book.genre === gen).length})
+              </label>
+            </div>)}
+        </fieldset>
+        <fieldset>
+          <label className='text-base text-orange-800 font-bold'>
+            {pages} min pages
+          </label>
+          <input
+            type='range'
+            min={0} max={maxNumberOfPages}
+            id='pages'
+            onChange={handlePageChange}
+            value={pages}
+          />
+        </fieldset>
+      </div>
     </div>
   )
 }
