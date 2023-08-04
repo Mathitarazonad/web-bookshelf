78866ae3143daca95450897e074f1e198c6d2e9d
MathiTarazonad <vladitarazona@gmail.com>
2023-08-04 10:51:07 -0400

    Add type for localStorage keys

--- src/services/localStorage.ts
+++ src/services/localStorage.ts
@@ -1,4 +1,6 @@
-export const getFromLocalStorage = (key: string) => {
+export type LocalStorageKey = 'genres' | 'pages' | 'availableBooks' | 'toRead'
+
+export const getFromLocalStorage = (key: LocalStorageKey) => {
   if (window.localStorage.getItem(key) !== null) {
     return JSON.parse(window.localStorage.getItem(key) as string)
   }
@@ -6,6 +8,6 @@ export const getFromLocalStorage = (key: string) => {
   return null
 }
 
-export const updateLocalStorage = (key: string, item: unknown) => {
+export const updateLocalStorage = (key: LocalStorageKey, item: unknown) => {
   window.localStorage.setItem(key, JSON.stringify(item))
 }
